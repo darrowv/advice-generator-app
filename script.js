eb5ebcd7c54498e659c0bdf1154b3f6ef76cb0a5
Muhammad-Nasyr <nasirmaga45@gmail.com>
2022-12-01 00:24:00 +0300

    removed unsecure connection

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const adviceNode = document.getElementById("advice-text");
 const diceBtn = document.getElementById("dice-btn");
 
 const getRandomAdvice = async () =>  {
-  const res = await fetch("http://api.quotable.io/random");
+  const res = await fetch("https://api.quotable.io/random");
   const { content, length } = await res.json();
 
   adviceId.textContent = length;
