4b9c61110de1e102dbbc97ba011f202d0983e2c8
Muhammad-Nasyr <nasirmaga45@gmail.com>
2022-12-01 11:58:46 +0300

    added no-cache to fetch request

--- script.js
+++ script.js
@@ -2,20 +2,29 @@ const adviceId = document.getElementById("advice-id");
 const adviceNode = document.getElementById("advice-text");
 const diceBtn = document.getElementById("dice-btn");
 
-const getRandomAdvice = async () => {
-  const res = await fetch("https://api.adviceslip.com/advice", {
-    method: "GET",
-    headers: {
-      "Content-type": "text/plain",
-    },
-  });
-
-  const { slip } = await res.json();
-
-  adviceId.textContent = slip.id;
-  adviceNode.textContent = slip.advice;
-};
-
 diceBtn.addEventListener("click", getRandomAdvice);
 
 window.onload = getRandomAdvice();
+
+async function getRandomAdvice() {
+  try {
+    const res = await fetch("https://api.adviceslip.com/advice", {
+      method: "GET",
+      cache: "no-cache",
+      headers: {
+        "Content-type": "text/plain",
+      },
+    });
+
+    const { slip } = await res.json();
+
+    setContent(slip.id, slip.advice);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function setContent(id, advice) {
+  adviceId.textContent = id;
+  adviceNode.textContent = advice;
+}
